64f0b0ae820bc1dec09409e9019fa86adafde66c
Gary Deschaines <gedeschaines@gmail.com>
2022-01-29 21:00:11 -0600

    Deactivate light sensor gain and set Tau=1 and evalTime=2000 to generate rubustly fit robot.

--- phototaxis/constants.py
+++ phototaxis/constants.py
@@ -2,13 +2,14 @@
     https://www.reddit.com/r/ludobots/wiki/pyrosim/phototaxis
 """
 # Robot design parameters
-L = 0.4   # Leg length
-R = L/5   # Leg radius
-D = 10*L  # lidht sensor distance
-G = 10    # light sensor gain
+L = 0.4    # leg length
+R = L/5    # leg radius
+D = 30*L   # light sensor distance
+G = 1.0    # light sensor gain
+Tau = 1.0  # motor neuron time constant
 
 # Evolution simulation parameters
-evalTime = 500   # number of time steps per simulation
+evalTime = 2000  # number of time steps per simulation
 popSize  = 10    # size of population
 numGens  = 200   # number of generations
 numEnvs  = 4     # number of environments
